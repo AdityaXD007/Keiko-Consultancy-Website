e9793ac9947e192a55f86ad7ccdf2087f64180a6
AdityaXD007 <karkikaaditya12345@gmail.com>
2026-08-07 15:02:15 +0545

    Fix STATIC_URL leading slash for production

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (Uploaded images)
